8fef737083b908872ef658df783e8a902b3a7a99
Himanshu Tyagi <recoteco567@gmail.com>
2021-01-15 22:53:05 +0530

    Add Fluid Stuff

--- fluid_RMX2001.mk
+++ fluid_RMX2001.mk
@@ -21,7 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2001/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common FluidOS stuff.
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+FLUID_BUILD_TYPE := UNOFFICIAL
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.fluid.maintainer=ManshuTyagi \
+  ro.fluid.cpu=Mediatek-G90T
 $(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
